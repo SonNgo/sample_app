17aad9dd629612b5d18b32cc59a50fbd4c80091e
nts <ngotungson0602@gmail.com>
2015-07-19 20:21:00 +0700

    follow-user

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create!(name:  "nts",
              password_confirmation: "856329",
              admin: true)
 
-99.times do |n|
+40.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -27,3 +27,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+
+users = User.all
+user  = users.first
+following = users[2..10]
+followers = users[3..10]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
